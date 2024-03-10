c45df2569dd01467509537109683860e7ddf0f4d
Reggie1998 <Regimantas1998@gmail.com>
2024-03-10 11:01:33 +0100

    aaaa

--- Assets/Scripts/Player/InputsController.cs
+++ Assets/Scripts/Player/InputsController.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(ThirdPersonController))]
 public class InputsController : MonoBehaviour
 {
+    MovementHandler movementHandler;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        var movementInput = value.Get<Vector2>();
+
+        //movementHandler.OnMovementPressed(movementInput);
+        //Debug.Log(value.Get<Vector2>());
     }
 
     public void Jump()
@@ -30,5 +33,51 @@ public class InputsController : MonoBehaviour
     public void OnJump()
     {
         Debug.Log("OnJump");
+    }
+}
+
+public class MovementHandler
+{
+    private Vector3 movementVector;
+    private bool inDash = false;
+    private Vector2 dashVelocity;
+    private float verticalVelocity;
+    private bool trackedIsGrounded;
+    private float movementSpeed = 10.0f;
+    private Vector2 lastMovementInputVelocity;
+    private bool movementInput;
+    public CharacterController charController;
+
+
+    public Vector2 movementInputVelocity;
+
+    public void HandleMovement()
+    {
+        movementVector = new Vector3(0.0f, verticalVelocity, 0.0f);
+        if (inDash) {
+            movementVector.x = dashVelocity.x;
+            movementVector.z = dashVelocity.y;
+        } else if (!trackedIsGrounded) {
+            var lateralMovement = new Vector2(movementInputVelocity.x * movementSpeed, movementInputVelocity.y * movementSpeed);
+            if (lastMovementInputVelocity != Vector2.zero) {
+                var lerpedMovement = Vector2.Lerp(lastMovementInputVelocity, lateralMovement, 0.8f);
+                movementVector = new Vector3(lerpedMovement.x, verticalVelocity, lerpedMovement.y);
+            } else {
+                movementVector.x = lateralMovement.x;
+                movementVector.z = lateralMovement.y;
+            }
+        } else if (movementInput) {
+            movementVector.x = movementInputVelocity.x * movementSpeed;
+            movementVector.z = movementInputVelocity.y * movementSpeed;
+        }
+
+        charController.Move(movementVector * Time.deltaTime);
+    }
+
+    public void OnMovementPressed(Vector2 inputVal)
+    {
+        movementInput = true;
+        movementInputVelocity = inputVal.normalized;
+        //lastMovementInputVelocity = Vector2.zero;
     }
 }
---
+++ Assets/Scripts/Player/ThirdPersonController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
+    public Transform combatLookAt;
+
+    public GameObject thirdPersonCam;
+    public GameObject combatCam;
+    public GameObject topDownCam;
+
+    public CameraStyle currentStyle;
+    public enum CameraStyle
+    {
+        Basic,
+        Combat,
+        Topdown
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate orientation
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+        orientation.forward = dirToCombatLookAt.normalized;
+
+        playerObj.forward = dirToCombatLookAt.normalized;
+    }
+}
